c55f66b1fb390c54cce4a5edfa31d44681c8179e
Brandon Francis <brandon@brandon-francis.com>
2013-11-10 20:45:18 -0500

    Initial commit
    Includes the ChromeTabControl as well as an example on how to use it.

---
+++ DotNetChromeTabs/TestForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DotNetChromeTabs
+{
+    public partial class TestForm : Form
+    {
+        private ChromeTabControl control;
+
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+
+            // Let's create a new tab control
+            control = new ChromeTabControl();
+
+            // Let's make the top bar transparent and look better
+            ChromeTabControl.AeroFunctions.ChromifyWindow(this, control);
+
+            // Add a handler for when the new tab button is clicked
+            control.NewTabClicked += control_NewTabClicked;
+
+            // Let's add it to the form
+            control.Dock = DockStyle.Fill;
+            this.Controls.Add(control);
+
+            // Let's add 2 test pages to it
+            control.TabPages.AddNoAnimate(new TestPage());
+            control.TabPages.AddNoAnimate(new TestPage());
+
+        }
+
+        void control_NewTabClicked(object sender, EventArgs e)
+        {
+            // Let's add in a new tab page
+            control.TabPages.Add(new TestPage());
+        }
+
+
+    }
+}
---
+++ DotNetChromeTabs/TestPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetChromeTabs
+{
+
+    /// <summary>
+    /// Example tab page.
+    /// </summary>
+    public partial class TestPage : ChromeTabControl.TabPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TestPage_Load(object sender, EventArgs e)
+        {
+        }
+
+        internal override bool NewInstanceAttempted(ChromeTabControl.TabPage newInstance)
+        {
+            return true;
+        }
+
+    }
+}
